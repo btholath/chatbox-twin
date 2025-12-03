d8f272636f2bdffe806f7e505289bee470d92d88
Biju Tholath <biju.tholath@gmail.com>
2025-12-03 13:49:48 -0800

    Fix input focus issue and add avatar support

--- frontend/components/twin.tsx
+++ frontend/components/twin.tsx
@@ -119,7 +119,7 @@ export default function Twin() {
                     <div className="text-center text-gray-500 mt-8">
                         {hasAvatar ? (
                             <img
-                                src="/avatar.png"
+                                src="/avatar.jpg"
                                 alt="Digital Twin Avatar"
                                 className="w-20 h-20 rounded-full mx-auto mb-3 border-2 border-gray-300"
                             />
@@ -142,7 +142,7 @@ export default function Twin() {
                             <div className="flex-shrink-0">
                                 {hasAvatar ? (
                                     <img
-                                        src="/avatar.png"
+                                        src="/avatar.jpg"
                                         alt="Digital Twin Avatar"
                                         className="w-8 h-8 rounded-full border border-slate-300"
                                     />
@@ -186,7 +186,7 @@ export default function Twin() {
                         <div className="flex-shrink-0">
                             {hasAvatar ? (
                                 <img
-                                    src="/avatar.png"
+                                    src="/avatar.jpg"
                                     alt="Digital Twin Avatar"
                                     className="w-8 h-8 rounded-full border border-slate-300"
                                 />
